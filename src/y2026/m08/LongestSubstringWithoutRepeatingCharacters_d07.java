c1257dd349a59fcede501d6a5f321013bc8cfba8
hyeongu01 <cdy5212@gmail.com>
2026-08-07 12:18:24 +0900

    add: 20260807 3. Longest Substring Without Repeating Characters

---
+++ src/y2026/m08/LongestSubstringWithoutRepeatingCharacters_d07.java
@@ -0,0 +1,106 @@
+package y2026.m08;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * link: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+ */
+public class LongestSubstringWithoutRepeatingCharacters_d07 {
+    static HashMap<String, Integer> testCases = new HashMap<>();
+
+    public static void main(String[] args) {
+        testCases.put("abccbad", 4);
+        testCases.put("abcabcbb", 3);
+        testCases.put("bbbbb", 1);
+        testCases.put("pwwkew", 3);
+
+        for (Map.Entry<String, Integer> entry: testCases.entrySet()) {
+            int result = lengthOfLongestSubstring(entry.getKey());
+            System.out.print(result == entry.getValue() ? " | 정답 | " : " | 오답 | ");
+            System.out.println("input = " + entry.getKey() + " result = " + result + " correct = " + entry.getValue());
+        }
+    }
+
+    /**
+     * 중복된 문자열을 만나고 이전 문자열로 돌아가서 봤던 것들을 또 순휘하여 시간복잡도가 커짐
+     * @param s
+     * @return
+     */
+//    static int lengthOfLongestSubstring(String s) {
+//        char[] arr = s.toCharArray();
+//        int result = 0;
+//
+//        HashMap<Character, Integer> map = new HashMap<>();
+//        int count  = 0, cursor = 0;
+//        while (cursor < arr.length) {
+//            if (!map.containsKey(arr[cursor])) {
+//                count ++;
+//                map.put(arr[cursor], cursor);
+//            } else {
+//                int target = map.get(arr[cursor]);
+//                map.clear();
+//                result = Math.max(result, count);
+//                cursor = target;
+//                count = 0;
+//            }
+//            cursor++;
+//        }
+//        result = Math.max(result, count);
+//
+//        return result;
+//    }
+
+    /**
+     * 시간복잡도: O(n), 공간복잡도: O(1)
+     * @param s
+     * @return
+     */
+    static int lengthOfLongestSubstring(String s) {
+        char[] arr = s.toCharArray();
+        int result = 0;
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        int startIndex = 0;
+        for (int i = 0; i < arr.length; i++) {
+            Integer prevIndex = map.get(arr[i]);
+
+            if (prevIndex != null && prevIndex >= startIndex) {
+                startIndex = prevIndex + 1;
+            }
+            map.put(arr[i], i);
+            result = Math.max(result, i - startIndex + 1);
+        }
+        return result;
+    }
+
+    /**
+     * 코드리뷰 이후: HashMap -> int[]
+     * @param s
+     * @return
+     */
+//    static int lengthOfLongestSubstring(String s) {
+//        int result = 0;
+//        int[] lastIndex = new int[128]; // 표준 아스키 코드 범위
+//        Arrays.fill(lastIndex, -1);
+//
+//        int startIndex = 0;
+//        for (int i = 0; i < s.length(); i++) {
+//            char c = s.charAt(i);
+//
+//            if (lastIndex[c] >= startIndex) {
+//                startIndex = lastIndex[c] + 1;
+//            }
+//            lastIndex[c] = i;
+//            result = Math.max(result, i - startIndex + 1);
+//        }
+//        return result;
+//    }
+}
+
+/**
+ * 코드 리뷰 이후
+ *
+ * 중복되지 않도록 문자를 처리할 때 HashMap 을 사용했는데, 아스키코드를 배열로 다루는 방법도 있다고 한다. (마지막 방법 참조)
+ */
