ebe6de2a431f9884ba0593a279511ebfa9bec8e0
hyeongu01 <cdy5212@gmail.com>
2026-08-13 09:55:21 +0900

    add:

--- src/y2026/m08/D13_ExclusiveTimeOfFunctions.java
+++ src/y2026/m08/D13_ExclusiveTimeOfFunctions.java
@@ -3,28 +3,17 @@ package y2026.m08;
 import utils.TestCase;
 import utils.TestRunner;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Stack;
-
-class Log {
-    int functionId;
-    int type; // 0: start, 1: end
-    int timestamp;
-
-    public Log(String log) {
-        String[] list = log.split(":");
-        functionId = Integer.parseInt(list[0]);
-        type = list[1].equals("start") ? 0 : 1;
-        timestamp = Integer.parseInt(list[2]);
-    }
-}
+import java.util.*;
 
 /**
  * https://leetcode.com/problems/exclusive-time-of-functions/
  * <p>
  * 자료구조: Stack
  * 접근 방식: 괄호와 같이 end 신호가 오면 start 신호를 함께 제거하며 시간을 증/감
+ *
+ * 개선사항:
+ * - 기존 Stack class 를 사용하여 스택을 사용함.
+ * - java.util.Stack 은 캡슐화가 깨져있고, 순회 방식이 Stack 과 달라 주의가 필요하다. ArrayDeque class 를 사용하는 것이 권장됨.
  */
 public class D13_ExclusiveTimeOfFunctions {
 
@@ -40,23 +29,32 @@ public class D13_ExclusiveTimeOfFunctions {
 
     }
 
+    private static class Log {
+        int functionId;
+        boolean isStart;
+        int timestamp;
+
+        public Log(String log) {
+            String[] list = log.split(":");
+            functionId = Integer.parseInt(list[0]);
+            isStart = list[1].equals("start");
+            timestamp = Integer.parseInt(list[2]);
+        }
+    }
+
     public int[] exclusiveTime(int n, List<String> logs) {
         int[] runningTimes = new int[n];
-        Stack<Log> runningStack = new Stack<>();
+        Deque<Log> runningStack = new ArrayDeque<>();
 
         for (String log : logs) {
             Log l = new Log(log);
-
-            // 새 작업이 들어오면 작업 스택에 추가
-            // end 신호가 들어오면 스택의 작업을 제거하고 동작 시각을 기록 (괄호처럼 짝이 맞아야함.)
-            // 이전 작업물이 있는 경우 중단된 시간만큼 runningTimes 를 감소
-            if (l.type == 0) {
+            if (l.isStart) {
                 runningStack.push(l);
             } else {
                 Log startLog = runningStack.pop();
                 int runtime = l.timestamp - startLog.timestamp + 1;
                 runningTimes[startLog.functionId] += runtime;
-                if (!runningStack.empty()) {
+                if (!runningStack.isEmpty()) {
                     Log prevLog = runningStack.peek();
                     runningTimes[prevLog.functionId] -= runtime;
                 }
