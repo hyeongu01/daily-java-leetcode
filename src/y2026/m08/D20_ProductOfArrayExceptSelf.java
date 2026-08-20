3b92d319ffb1dd3bcec01853741ba65c8febb6eb
hyeongu01 <cdy5212@gmail.com>
2026-08-20 18:09:53 +0900

    add:

---
+++ src/y2026/m08/D20_ProductOfArrayExceptSelf.java
@@ -0,0 +1,42 @@
+package y2026.m08;
+
+import utils.TestCase;
+import utils.TestRunner;
+
+public class D20_ProductOfArrayExceptSelf {
+
+    public static void main(String[] args) {
+        D20_ProductOfArrayExceptSelf solution = new D20_ProductOfArrayExceptSelf();
+
+        TestRunner.run("Product of Array Except Self",
+                solution::productExceptSelf,
+                new TestCase<>(new int[]{1, 2, 3, 4}, new int[]{24, 12, 8, 6}),
+                new TestCase<>(new int[]{-1, 1, 0, -3, 3}, new int[]{0, 0, 9, 0, 0}),
+                new TestCase<>(new int[]{-1, 0, 0, -3, 3}, new int[]{0, 0, 0, 0, 0})
+        );
+    }
+
+    public int[] productExceptSelf(int[] nums) {
+        int totalProduct = 1;
+        int zeroCount = 0;
+        for (int n : nums) {
+            if (n == 0) {
+                zeroCount += 1;
+                continue;
+            }
+            totalProduct *= n;
+        }
+
+        int[] result = new int[nums.length];
+        if (zeroCount == 0) {
+            for (int i = 0; i < nums.length; i++) {
+                result[i] = totalProduct / nums[i];
+            }
+        } else if (zeroCount == 1) {
+            for (int i = 0; i < nums.length; i++) {
+                result[i] = nums[i] == 0 ? totalProduct : 0;
+            }
+        }
+        return result;
+    }
+}
